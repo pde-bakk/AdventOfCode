0dd28ffbdcb858ad8c5b9e39c61f60918c5d80bf
pde-bakk <pde-bakk@student.codam.nl>
2020-12-05 11:41:39 +0100

    effishensea

--- AoC2020/day05/part1.py
+++ AoC2020/day05/part1.py
@@ -1,12 +1,6 @@
-highest = seat = 0
+highest = 0
 with open("input") as myfile:
 	for line in myfile:
-		row = int(line[0:7].replace('F', '0').replace('B', '1'), 2)
-		column = int(line[7:-1].replace('L', '0').replace('R', '1'), 2)
-		seatid = row * 8 + column
-		# print(row, column)
-		if seatid > highest:
-			highest = seatid
-
+		highest = max(highest, int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
 print(highest)
 #896
