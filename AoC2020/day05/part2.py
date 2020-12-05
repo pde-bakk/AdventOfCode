a23079033c845ddc060aba4ebb1f0edb6dcf0a1a
pde-bakk <pde-bakk@student.codam.nl>
2020-12-05 11:41:39 +0100

    effishensea

--- AoC2020/day05/part2.py
+++ AoC2020/day05/part2.py
@@ -1,17 +1,7 @@
 highest = seat = 0
-mylist = list()
-for i in range(896 + 1):
-	mylist.append(i)
-
+mylist = [i for i in range(53, 896 + 1)]
 with open("input") as myfile:
 	for line in myfile:
-		row = int(line[0:7].replace('F', '0').replace('B', '1'), 2)
-		column = int(line[7:-1].replace('L', '0').replace('R', '1'), 2)
-		seatid = row * 8 + column
-		# print(row, column)
-		# print(seatid)
-		mylist.remove(seatid)
-
+		mylist.remove(int(line.replace('B', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
 print(mylist)
 #659
-
