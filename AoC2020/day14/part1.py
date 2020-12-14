6b247cc7ecff511ff2c00e2056fd9f2d86cbeeb2
pde-bakk <pde-bakk@student.codam.nl>
2020-12-14 15:01:40 +0100

    part 1 done

---
+++ AoC2020/day14/part1.py
@@ -0,0 +1,20 @@
+import re
+
+d = dict()
+rows = [x for x in open("input", 'r').read().split("\n")]
+total, value = 0, 0
+for row in rows:
+    if row[:4] == 'mask':
+        mask = row[7:]
+    else:
+        pos, value = map(int, re.findall(r'\d+', row))
+        bit = 1
+        for i in range(1, len(mask) + 1):
+            if mask[-i] == '0' and value & bit > 0:
+                value -= bit
+            elif mask[-i] == '1' and value & bit == 0:
+                value += bit
+            bit *= 2
+        d[pos] = value
+
+print(sum(d.values()))
