41c8267730c85317f928d7afd5a3e56739d52994
pde-bakk <pde-bakk@student.codam.nl>
2020-12-14 18:57:09 +0100

    tried some nifty bitwise operations but its way slower apparently

--- AoC2020/day14/part1.py
+++ AoC2020/day14/part1.py
@@ -12,6 +12,7 @@ for row in rows:
         for i in range(1, len(mask) + 1):
             if mask[-i] == '0' and value & bit > 0:
                 value -= bit
+                # value &= ~(1 << bit) ## I wanted to use this, but it's way slower with big integers
             elif mask[-i] == '1' and value & bit == 0:
                 value += bit
             bit *= 2
