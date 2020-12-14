c306791644aca55fdcf4f6780860b464671c2b22
Peer de Bakker <pde-bakk@student.codam.nl>
2020-12-14 23:57:24 +0100

    now with extra Bitwise operations and comments

--- AoC2020/day14/part1.py
+++ AoC2020/day14/part1.py
@@ -8,14 +8,26 @@ for row in rows:
         mask = row[7:]
     else:
         pos, value = map(int, re.findall(r'\d+', row))
-        bit = 1
         for i in range(1, len(mask) + 1):
-            if mask[-i] == '0' and value & bit > 0:
-                value -= bit
-                # value &= ~(1 << bit) ## I wanted to use this, but it's way slower with big integers
-            elif mask[-i] == '1' and value & bit == 0:
-                value += bit
-            bit *= 2
+            if mask[-i] == '0':
+                value &= ~(1 << i - 1)
+            elif mask[-i] == '1':
+                value |= (1 << i - 1)
         d[pos] = value
 
 print(sum(d.values()))
+
+# This here is my addition code, but I also wanted to make it work with bitwise operations
+# for row in rows:
+# 	if row[:4] == 'mask':
+# 		mask = row[7:]
+# 	else:
+# 		pos, value = map(int, re.findall(r'\d+', row))
+# 		bit = 1
+# 		for i in range(1, len(mask) + 1):
+# 			if mask[-i] == '0' and value & bit > 0:
+# 				value -= bit
+# 			elif mask[-i] == '1' and value & bit == 0:
+# 				value += bit
+# 			bit *= 2
+# 		d[pos] = value
