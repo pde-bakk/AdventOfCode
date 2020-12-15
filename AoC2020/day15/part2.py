de950130812ad44e9169558b329d091bc376c3e7
Peer de Bakker <pde-bakk@student.codam.nl>
2020-12-15 23:29:42 +0100

    optimised in python, why ever

--- AoC2020/day15/part2.py
+++ AoC2020/day15/part2.py
@@ -2,14 +2,11 @@ nbs = [int(x) for x in open('input', 'r').read().split(',')]
 d = dict()
 for i in range(len(nbs) - 1):
 	d[nbs[i]] = i
-
-while len(nbs) < 30000000:
-	if nbs[-1] not in d:
-		# print("{} is not in dictionary yet".format(nbs[-1]))
-		nbs.append(0)
-	else:
-		nbs.append(len(nbs) - 1 - d[nbs[-1]])
-	d[nbs[-2]] = len(nbs) - 2
-	if len(nbs) % 3000000 == 0:
-		print("10% done")
-print(nbs[-1])
+val = nbs[-1]
+# print(d)
+for i in range(len(nbs) - 1, 30000000 - 1):
+	try:
+		d[val], val = i, i - d[val]
+	except KeyError:
+		d[val], val = i, 0
+print(val)
