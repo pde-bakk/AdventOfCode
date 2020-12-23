fa62bd4b5640512b158e45511345d0c19f87d223
Peer de Bakker <pde-bakk@student.codam.nl>
2020-12-23 22:36:45 +0100

    got stuck on some bs

--- AoC2020/day23/day.py
+++ AoC2020/day23/day.py
@@ -1,45 +1,50 @@
 mycups = [9, 5, 2, 3, 1, 6, 4, 8, 7]
-mycups = [3, 8, 9, 1, 2, 5, 4, 6, 7]  # sample
+# mycups = [3, 8, 9, 1, 2, 5, 4, 6, 7]  # sample
 
 
-def game(cups, gamelength):
-	cur = 0
+def game(cups, currentcup, gamelength):
 	for i in range(1, gamelength + 1):
-		print(f'-- move {i} --')
-		currentcup = cups[cur % len(cups)]
-		cur = cups.index(currentcup)
-		# print(f'cups: {cups} ({currentcup})')
-		threecups = cups[cur + 1: cur + 4]
-		if len(threecups) < 3:
-			threecups += cups[:3 - len(threecups)]
-		# print(f'pick up: {threecups}')
-		cups = [x for x in cups if x not in threecups]
-		# cups = cups[:cur + 1] + cups[cur + 4:]
+		# print(f'-- move {i} --')
+		pickone = cups[currentcup]
+		picktwo = cups[pickone]
+		pickthree = cups[picktwo]
+		cups[currentcup] = cups[pickthree]
+
 		destination = currentcup - 1
-		while True:
-			if destination in cups:
-				break
-			if destination == 0:
-				destination = max(cups)
-			else:
-				destination -= 1
+		while destination in [pickone, picktwo, pickthree] or destination < 1:
+			destination -= 1
+			if destination < 1:
+				destination = max(cups.values())
 		# print(f'destination: {destination}\n')
-		cups = cups[:cups.index(destination) + 1] + threecups + cups[cups.index(destination) + 1:]
-		cur = cups.index(currentcup) + 1
+
+		cups[pickthree] = cups[destination]
+		cups[destination] = pickone
+		currentcup = cups[currentcup]
+	print(cups[1] * cups[cups[1]])
 	return cups
 
 
 def score(cups):
 	print(f'-- final --')
+	st, tmp = str(), cups[1]
 	print(f'cups: {cups}')
-	s = ''.join([str(x) for x in cups]).split('1')
-	return s[1] + s[0]
+	while tmp != 1:
+		st += str(tmp)
+		tmp = cups[tmp]
+	return st
 
 
-# print(f'Part 1: {score(game(mycups, 100))}')
-maxcup = max(mycups)
-extracups = [x for x in range(max(mycups), 1000000)]
-print(len(extracups) + len(mycups))
-ret = game(mycups + extracups, 10)
-after = ret[ret.index(1):]
-print(after)
+def score2(cups):
+	print(f'-- final --')
+	print(f'cups[1]: {cups[1]}, cups[{cups[1]}]: {cups[cups[1]]}')
+	return cups[1] * cups[cups[1]]
+
+
+d = {k: mycups[i+1] for i, k in enumerate(mycups) if i < len(mycups) - 1}
+d[mycups[-1]] = mycups[0]
+print(f'Part 1: {score(game(d, mycups[0], 100))}')
+extracups = [x for x in range(10, 1000000 + 1)]
+mycups += extracups
+d = {k: mycups[i+1] for i, k in enumerate(mycups) if i < len(mycups) - 1}
+d[mycups[-1]] = mycups[0]
+print(score2(game(d, mycups[0], 10000000)))
