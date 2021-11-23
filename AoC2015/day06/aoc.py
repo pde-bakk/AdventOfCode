9f5e4a85547cd4a91ca67f0de206f680e95c4226
Peer de Bakker <pde-bakk@student.codam.nl>
2021-11-23 17:58:18 +0100

    part 2 done

--- AoC2015/day06/aoc.py
+++ AoC2015/day06/aoc.py
@@ -19,9 +19,28 @@ def part1(rows: List[str]) -> int:
 	return sum(sum(light for light in row) for row in lights)
 
 
+def part2(rows: List[str]) -> int:
+	lights = [[0] * 1000 for _ in range(1000)]
+	for row in rows:
+		if not row:
+			break
+		x1, y1, x2, y2 = [int(s) for s in re.findall(r'\b\d+\b', row)]
+		for y in range(y1, y2 + 1):
+			for x in range(x1, x2 + 1):
+				if 'turn on' in row:
+					lights[y][x] += 1
+				elif 'turn off' in row:
+					lights[y][x] = max(0, lights[y][x] - 1)
+				elif 'toggle' in row:
+					lights[y][x] += 2
+	return sum(sum(light for light in row) for row in lights)
+
+
 assert part1(['turn on 0,0 through 999,0']) == 1000
 assert part1(['toggle 499,499 through 500,500']) == 4
 input_rows = open('input.txt', 'r').read().split('\n')
 print(part1(input_rows))
 
 # Part 2
+
+print(part2(input_rows))
