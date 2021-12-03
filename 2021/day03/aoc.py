c90d9732fc98e27f92eaa2eb7847d17ea242ae08
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-03 08:24:19 +0100

    lekker yusha

--- 2021/day03/aoc.py
+++ 2021/day03/aoc.py
@@ -10,12 +10,16 @@ def find_most_common_value(r: list, idx: int) -> str:
 	return '1'
 
 
+def invert_value(bit: str) -> str:
+	return str(int(not int(bit)))
+
+
 def part1():
 	gamma, epsilon = '', ''
 	for i in range(len(rows[0])):
 		most_common = find_most_common_value(rows, i)
 		gamma += most_common
-		epsilon += str(int(not int(most_common)))
+		epsilon += invert_value(most_common)
 	print(f'Gamma: {gamma} ({int(gamma, 2)}), Epsilon: {epsilon} ({int(epsilon, 2)})')
 
 	return int(gamma, 2) * int(epsilon, 2)
@@ -28,7 +32,7 @@ def get_rating(r: list, co2: bool):
 			break
 		currbit = find_most_common_value(r, i)
 		if co2:
-			currbit = str(int(not int(currbit)))
+			currbit = invert_value(currbit)
 		r = [row for row in r if row[i] == currbit]
 	return r[0]
 
@@ -36,11 +40,10 @@ def get_rating(r: list, co2: bool):
 def part2(r: list):
 	oxygen = get_rating(r, False)
 	co2 = get_rating(r, True)
-	print(oxygen, co2)
 	print(f'oxygen: {oxygen} ({int(oxygen, 2)}), co2: {co2} ({int(co2, 2)})')
 	return int(oxygen, 2) * int(co2, 2)
 
 
 rows = open('input.txt').read().splitlines()
-# print(f'part1: {part1()}')
+print(f'part1: {part1()}')
 print(f'part2: {part2(rows)}')
