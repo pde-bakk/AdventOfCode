dfa7cbad91d8383599f67285fa88e0a1204423ce
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-04 12:24:18 +0100

    part1 done using numpy

---
+++ 2021/day04/aoc.py
@@ -0,0 +1,61 @@
+import numpy as np
+import math
+
+
+class Board:
+	def __init__(self, string):
+		self.matrix = []
+		nbs = [int(x) for x in string.split()]
+		size = int(math.sqrt(len(nbs)))
+		self.matrix = np.array(nbs, dtype=np.int16).reshape((size, size))
+
+	def mark_nb(self, nb: int):
+		found = np.where(self.matrix == nb)
+		try:
+			y, x = found[0][0], found[1][0]
+			print(f'found {nb} at {x, y}')
+			self.matrix[y][x] = -1
+		except IndexError:
+			pass
+
+	def is_solved(self) -> bool:
+		for i in range(self.matrix.shape[0]):
+			# print(f'self.matrix[{i}] = {self.matrix[i]}')
+			if np.all(self.matrix[i] == self.matrix[i][0]):  # row i contains only marked numbers
+				return True
+			# print(f'self.matrix[:{i}] = {self.matrix[:, i]}')
+			if np.all(self.matrix[:, i] == self.matrix[:, i][0]):  # column i contains only marked nbs
+				return True
+		return False
+
+	def get_score(self):
+		total = 0
+		for item in np.ndenumerate(self.matrix):
+			if item[1] != -1:
+				print(f'adding {item[1]}')
+				total += item[1]  # item[0] is the index
+		return total
+
+
+def setup_boards():
+	for b in lines:
+		boards.append(Board(b))
+
+
+def mark_numbers():
+	for x in nbs_todraw:
+		for b in boards:
+			b.mark_nb(x)
+
+		for b in boards:
+			if b.is_solved():
+				return b.get_score() * x
+	return 0
+
+
+lines = open('input.txt').read().split('\n\n')
+nbs_todraw = [int(x) for x in lines.pop(0).split(',')]
+print(lines)
+boards = []
+setup_boards()
+print(f'Part1: {mark_numbers()}')
