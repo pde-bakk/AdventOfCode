2902d38b72bbeed46e2a1e46c2f723ac6d941c62
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-07 07:06:25 +0100

    smart solution

--- 2021/day07/aoc.py
+++ 2021/day07/aoc.py
@@ -1,13 +1,12 @@
 lines = open("input.txt").readline().split(',')
 poses = [int(x) for x in lines]
-print(poses)
+dists = {}
 
 
 def part1():
 	total = 999999999999
 	for i in poses:
 		subtotal = sum([abs(x - i) for x in poses])
-		# print(f'subtotal = {subtotal}')
 		if subtotal < total:
 			total = subtotal
 	return total
@@ -19,10 +18,9 @@ def part2():
 		subtotal = 0
 		for x in poses:
 			diff = abs(x - i)
-			nonlineardiff = sum([x for x in range(diff + 1)])
-			# print(f'{x}=>{i} = {diff} becomes {nonlineardiff}')
-			subtotal += nonlineardiff
-		# print(f'subtotal = {subtotal}')
+			if diff not in dists:
+				dists[diff] = sum([x for x in range(diff + 1)])
+			subtotal += dists[diff]
 		if subtotal < total:
 			total = subtotal
 	return total
