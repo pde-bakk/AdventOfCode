55bcb77b76654936f65198e85c65a4e715e43d55
pde-bakk <pde-bakk@student.codam.nl>
2021-12-11 13:22:37 +0100

    flashy flashy

--- 2021/day11/aoc.py
+++ 2021/day11/aoc.py
@@ -7,48 +7,50 @@ def print_octos(listy, step: int = 0):
 		print(line)
 
 
-def day11(curr: list[list[int]]):
-	print_octos(curr)
+def day11(octos: list[list[int]]):
+	print_octos(octos)
 
 	def flash(yy: int, xx: int) -> int:
 		neighbours = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-		curr[yy][xx] = -1
+		octos[yy][xx] = -1
 		octoflash = 1
 
 		for n in neighbours:
 			ny, nx = n[0] + yy, n[1] + xx
-			if ny == -1 or nx == -1 or ny == len(curr) or nx == len(curr[ny]):
+			if ny == -1 or nx == -1 or ny == len(octos) or nx == len(octos[ny]):
 				continue
-			if curr[ny][nx] == -1:
+			if octos[ny][nx] == -1:
 				continue
-			curr[ny][nx] += 1
-			if curr[ny][nx] > 9:
+			octos[ny][nx] += 1
+			if octos[ny][nx] > 9:
 				octoflash += flash(ny, nx)
 		return octoflash
 
 	flashes = 0
 	for step in range(1000):
 		stepflashes = 0
-		for y, row in enumerate(curr):
+		# Increment energy levels of all octopi
+		for y, row in enumerate(octos):
 			for x, octo in enumerate(row):
-				curr[y][x] += 1
-		for y, row in enumerate(curr):
+				octos[y][x] += 1
+		# Flash!
+		for y, row in enumerate(octos):
 			for x, octo in enumerate(row):
-				if curr[y][x] > 9:
+				if octos[y][x] > 9:
 					stepflashes += flash(y, x)
 		flashes += stepflashes
 
 		# reset energy levels
-		for y, row in enumerate(curr):
+		for y, row in enumerate(octos):
 			for x, octo in enumerate(row):
-				if curr[y][x] == -1:
-					curr[y][x] = 0
-		print_octos(curr, step)
-		if stepflashes == len(curr) * len(curr[0]):
+				if octos[y][x] == -1:
+					octos[y][x] = 0
+		print_octos(octos, step)
+		if stepflashes == len(octos) * len(octos[0]):
 			print(f'Part2: {step + 1}')
 		if step == 100:
 			print(f'Part1: {flashes}')
 
 
-octos = [[int(x) for x in line] for line in open('input.txt').read().splitlines()]
-print(octos)
+grid = [[int(x) for x in line] for line in open('input.txt').read().splitlines()]
+day11(grid)
