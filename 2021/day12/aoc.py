f3621955f7090fff05d184fb350cf9197c035711
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-12 11:30:11 +0100

    part 2 was hella slow but w/e

--- 2021/day12/aoc.py
+++ 2021/day12/aoc.py
@@ -5,18 +5,21 @@ class Route:
 	def __init__(self, curr: str):
 		self.visited = [curr]
 		self.current_node = curr
+		self.visited_small_cave_twice = False
 
 	def __deepcopy__(self, memodict={}):
 		res = Route(self.current_node)
 		res.visited = deepcopy(self.visited)
 		return res
 
-	def move(self, newnode: str):
+	def move(self, newnode: str) -> None:
 		self.visited.append(newnode)
 		self.current_node = newnode
+		if not self.visited_small_cave_twice and any([node.islower() and self.visited.count(node) == 2 for node in self.visited]):
+			self.visited_small_cave_twice = True
 
 
-def parse_paths(rows: list[str]):
+def parse_paths(rows: list[str]) -> dict:
 	paths = dict()
 	for row in rows:
 		src, dst = row.split('-')
@@ -24,12 +27,14 @@ def parse_paths(rows: list[str]):
 			paths[src] = []
 		if dst not in paths:
 			paths[dst] = []
-		paths[src].append(dst)
-		paths[dst].append(src)
+		if dst != 'start' and src != 'end':
+			paths[src].append(dst)
+		if src != 'start' and dst != 'end':
+			paths[dst].append(src)
 	return paths
 
 
-def part1(rows: list[str], startnode='start'):
+def day12(rows: list[str], part: int, startnode='start') -> int:
 	paths = parse_paths(rows)
 	queue = [Route(startnode)]
 	finished = []
@@ -42,14 +47,17 @@ def part1(rows: list[str], startnode='start'):
 		# print(f'currently on {s.current_node}, already visited {s.visited}')
 
 		for n in paths[s.current_node]:
-			if n not in s.visited or n.isupper():
+			if n not in s.visited or n.isupper() or (part == 2 and not s.visited_small_cave_twice):
 				# print(f'appending {n}')
 				newroute = deepcopy(s)
 				newroute.move(n)
 				queue.append(newroute)
+	for i in finished:
+		print(i.visited)
 	return len(finished)
 
 
 lines = open('input.txt').read().splitlines()
 print(lines)
-print(f'Part 1: {part1(lines)}')
+# print(f'Part 1: {day12(lines, 1)}')
+print(f'Part 2: {day12(lines, 2)}')
