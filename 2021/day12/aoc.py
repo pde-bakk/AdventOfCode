0e8981bd4b2645c0e6e68d51745015c516cab86e
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-12 15:58:18 +0100

    removed deepcopying and list popping

--- 2021/day12/aoc.py
+++ 2021/day12/aoc.py
@@ -1,22 +1,20 @@
-from copy import deepcopy
+from collections import deque
 
 
 class Route:
-	def __init__(self, curr: str):
-		self.visited = [curr]
+	def __init__(self, curr: str, visited=None, smallcave=False):
 		self.current_node = curr
-		self.visited_small_cave_twice = False
-
-	def __deepcopy__(self, memodict={}):
-		res = Route(self.current_node)
-		res.visited = deepcopy(self.visited)
-		return res
+		if visited is None:
+			self.visited = {curr}
+		else:
+			self.visited = visited.copy()
+		self.visited_small_cave_twice = smallcave
 
 	def move(self, newnode: str) -> None:
-		self.visited.append(newnode)
-		self.current_node = newnode
-		if not self.visited_small_cave_twice and any([node.islower() and self.visited.count(node) == 2 for node in self.visited]):
+		if not self.visited_small_cave_twice and newnode.islower() and newnode in self.visited:
 			self.visited_small_cave_twice = True
+		self.visited.add(newnode)
+		self.current_node = newnode
 
 
 def parse_paths(rows: list[str]) -> dict:
@@ -36,28 +34,23 @@ def parse_paths(rows: list[str]) -> dict:
 
 def day12(rows: list[str], part: int, startnode='start') -> int:
 	paths = parse_paths(rows)
-	queue = [Route(startnode)]
+	queue = deque()
+	queue.append(Route(startnode))
 	finished = []
 
 	while queue:
-		s = queue.pop(0)
+		s = queue.popleft()
 		if s.current_node == 'end':
-			# print(f'Found solid route: {s.visited}')
 			finished.append(s)
-		# print(f'currently on {s.current_node}, already visited {s.visited}')
 
 		for n in paths[s.current_node]:
 			if n not in s.visited or n.isupper() or (part == 2 and not s.visited_small_cave_twice):
-				# print(f'appending {n}')
-				newroute = deepcopy(s)
+				newroute = Route(s, s.visited, s.visited_small_cave_twice)
 				newroute.move(n)
 				queue.append(newroute)
-	for i in finished:
-		print(i.visited)
 	return len(finished)
 
 
 lines = open('input.txt').read().splitlines()
-print(lines)
-# print(f'Part 1: {day12(lines, 1)}')
+print(f'Part 1: {day12(lines, 1)}')
 print(f'Part 2: {day12(lines, 2)}')
