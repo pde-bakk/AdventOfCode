c372c752a5207af2b0220d180ba9161446f5ff11
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-13 10:33:14 +0100

    that part 2 tho

--- 2021/day13/aoc.py
+++ 2021/day13/aoc.py
@@ -1,38 +1,57 @@
 import numpy as np
 
 
-def part1(matrix):
-	print()
-	for f in folds:
-		if f[0] == 'y':
-			for y in range(size):
-				print(f'f = {f}')
-				if f[1] + y >= matrix.shape[1]:
-					continue
-				matrix[f[0] - y] |= matrix[f[0] + y]
-				for ye in range(y, size):
-					matrix = np.delete(matrix, ye)
-	print(matrix)
+def day13(matrix, part) -> int:
+	for fold in fold_input.split('\n'):
+		f = fold.split('=')
+		# print(f'f={f}')
+		if f[0][-1] == 'y':
+			fy = int(f[1])
+			# print(f'fy={fy}')
+			# print(f'shape = {matrix.shape}')
+			for y in range(1, ymax):
+				if fy + y >= matrix.shape[0]:
+					break
+				# print(f'folding {fy+y} onto {fy-y}')
+				# print(f'folding {matrix[fy+y]} onto {matrix[fy-y]}')
+				matrix[fy - y] |= matrix[fy + y]
+			matrix = matrix[:fy]
+		elif f[0][-1] == 'x':
+			fx = int(f[1])
+			for x in range(1, xmax):
+				if fx + x >= matrix.shape[1]:
+					break
+				matrix[:, fx - x] |= matrix[:, fx + x]
+			matrix = matrix[:, 0:fx]
+		# print(matrix, matrix.sum(), end='\n\n')
+		if part == 1:
+			break
+	if part == 2:
+		for row in matrix:
+			for c in row:
+				if c == 0:
+					print(' ', end='')
+				else:
+					print('#', end='')
+			print()
+	return matrix.sum()
 
 
 dots, fold_input = open('input.txt').read().split('\n\n')
 dots = [[int(x) for x in row.split(',')] for row in dots.split('\n')]
-folds = []
-for row in fold_input.split('\n'):
-	if not row:
-		continue
-	r = row.split('=')
-	folds += tuple([r[0][-1], int(r[1])])
-
-size = 15
-print(size)
-grid = np.zeros(shape=(size, size), dtype=np.uint8)
+xmax, ymax = 0, 0
 for dot in dots:
+	xmax = max(xmax, dot[0] + 1)
+	ymax = max(ymax, dot[1] + 1)
+
+print(xmax, ymax)
+grid = np.zeros(shape=(ymax, xmax), dtype=np.uint8)
+for dot in dots:
+	print(f'dot={dot}')
 	grid[dot[1]][dot[0]] = 1
-print(dots)
-print(folds)
 for g in grid:
 	print(g)
 
-print(f'here')
-part1(grid)
+print(f'here, range(10) = {range(10)}')
+print(f'Part1: {day13(grid, part=1)}')
+print(f'Part2: {day13(grid, part=2)}')
