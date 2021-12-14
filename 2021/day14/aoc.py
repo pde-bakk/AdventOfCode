d141b214a0c7f0d751a6c446f022f08ce119ee1e
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-14 10:53:21 +0100

    Hi there lanternfish

--- 2021/day14/aoc.py
+++ 2021/day14/aoc.py
@@ -1,31 +1,29 @@
-polymer, second = open('input.txt').read().split('\n\n')
+from collections import Counter
 
-polymer = [c for c in polymer]
+
+polymer, second = open('input.txt').read().split('\n\n')
 
 instructions = {c: {} for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'}
 # instructions = {'B': {}, 'C': {}, 'H': {}, 'N': {}}
 for inst in second.split('\n'):
 	a, b = inst.split(' -> ')
-	instructions[a[0]][a[1]] = b
+	instructions[a] = b
 
-for step in range(10):
-	newpolymer = []
-	for i in range(len(polymer) - 1):
-		first_char = polymer[i]
-		second_char = polymer[i + 1]
-		newpolymer.append(first_char)
-		# print(f'appending {first_char}')
-		if second_char in instructions[first_char]:
-			# print(f'appending {instructions[first_char]}')
-			newpolymer.append(instructions[first_char][second_char])
-		# print(f'appending {second_char}')
-		# newpolymer.append(second_char)
-	# print(f'appending last char: {polymer[-1]}')
-	newpolymer.append(polymer[-1])
-	print(f'After step {step + 1}: {newpolymer}')
-	polymer = newpolymer
+pairs = Counter((polymer[i:i+2]) for i in range(len(polymer) - 1))
+print(pairs)
 
-counts = sorted([polymer.count(c) for c in instructions.keys()])
-while 0 in counts:
-	counts.remove(0)
-print(counts, counts[-1] - counts[0])
+for step in range(40):
+	newnewpolymer = Counter()
+	for pair, count in pairs.items():
+		# print(f'pair={pair}, count={count}')
+		inbetweener = instructions[pair]
+		newnewpolymer[f'{pair[0]}{inbetweener}'] += count
+		newnewpolymer[f'{inbetweener}{pair[1]}'] += count
+	pairs = newnewpolymer
+
+counts = {k[0]: 0 for k in pairs}
+counts[polymer[-1]] += 1  # The last character never changes
+for k, v in pairs.items():
+	counts[k[0]] += v
+print(counts)
+print(max(counts.values()) - min(counts.values()))
