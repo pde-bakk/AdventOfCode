8e1479096c38296ad7be9775b5bfabc9c9188134
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-16 15:01:45 +0100

    cleaned up a lil, sweep sweep go brrrrroom

--- 2021/day16/aoc.py
+++ 2021/day16/aoc.py
@@ -14,70 +14,57 @@ class Packet:
 	def parse_packet(self, binary: str):
 		print(f'parse_packet: binary={binary}')
 		if not binary:
-			print('bye')
 			return 0
 		self.subpackets = []
 		self.version = int(binary[:3], 2)
 		self.type_id = int(binary[3:6], 2)
-		if self.version is None or self.type_id is None:
-			print(f'version woulda been {binary[:3]}, type_id woulda been {binary[3:6]}')
-		self.value = ''
-		print(f'version={self.version}, type_id={self.type_id}')
 		idx = 6
 		if self.type_id == 4:  # Literal value
-			firstbit = '1'
-			while firstbit == '1':
-				firstbit = binary[idx]
-				label = binary[idx + 1: idx + 5]
-				self.value += label
-				idx += 5
-				print(f'firstbit={firstbit}, label={label}, value={self.value}, idx={idx}')
-			self.value = int(self.value, 2)
-			print(f'setting value = {self.value}')
+			idx = self.parse_literal(binary, idx)
 		else:  # Operator packet
-			print(f'idx={idx}')
-			self.length_type_id = int(binary[idx])
-			idx += 1
-			print(f'length_type_id={self.length_type_id}')
-			if self.length_type_id == 0:
-				print(f'getting total bit length from {binary[idx:idx+15]}')
-				self.total_bit_len = int(binary[idx:idx+15], 2)
-				idx += 15
-				target = idx + self.total_bit_len
-				print(f'self.total_bitlen={self.total_bit_len}, end={target}, idx={idx}')
-				print(f'subpackets={binary[idx:target]}')
-				print(f'idx now is {idx}, binary[idx:]={binary[idx:]}')
-				# exit(1)
-				while idx < target:
-					subpacket = Packet()
-					print(f'{idx} < {target}')
-					idx += subpacket.parse_packet(binary[idx:])
-					print(f'new idx={idx}, binary[idx:]={binary[idx:]}')
-					self.subpackets.append(subpacket)
-			else:
-				self.nb_subpackets = int(binary[idx:idx+11], 2)
-				print(f'nb_subpackets ({binary[idx:idx+11]})={self.nb_subpackets}')
-				idx += 11
-				for _ in range(self.nb_subpackets):
-					subpacket = Packet()
-					idx += subpacket.parse_packet(binary[idx:])
-					self.subpackets.append(subpacket)
-		print(f'returning idx={idx}')
+			idx = self.extract_operator(binary, idx)
+		return idx
+
+	def extract_operator(self, binary, idx):
+		self.length_type_id = int(binary[idx])
+		idx += 1
+		if self.length_type_id == 0:
+			self.total_bit_len = int(binary[idx:idx + 15], 2)
+			idx += 15
+			target = idx + self.total_bit_len
+			while idx < target:
+				subpacket = Packet()
+				idx += subpacket.parse_packet(binary[idx:])
+				self.subpackets.append(subpacket)
+		else:
+			self.nb_subpackets = int(binary[idx:idx + 11], 2)
+			idx += 11
+			for _ in range(self.nb_subpackets):
+				subpacket = Packet()
+				idx += subpacket.parse_packet(binary[idx:])
+				self.subpackets.append(subpacket)
+		return idx
+
+	def parse_literal(self, binary, idx):
+		firstbit = '1'
+		self.value = ''
+		while firstbit == '1':
+			firstbit = binary[idx]
+			label = binary[idx + 1: idx + 5]
+			self.value += label
+			idx += 5
+		self.value = int(self.value, 2)
 		return idx
 
 	def get_versions(self) -> int:
 		res = self.version
-		print(f'Packet self has version: {self.version}, type_id: {self.type_id} and value {self.value}')
 		for p in self.subpackets:
-			# print(f'p has version: {p.version}, type_id: {p.type_id} and value {p.value}')
 			tmp = p.get_versions()
 			if tmp is not None:
 				res += tmp
-			# res += p.get_versions()
 		return res
 
 	def __lt__(self, other):
-		print(f'lt: self={self.getvalue()}, other={other.getvalue()}')
 		return self.getvalue() < other.getvalue()
 
 	def __gt__(self, other):
@@ -86,14 +73,7 @@ class Packet:
 	def __eq__(self, other):
 		return self.getvalue() == other.getvalue()
 
-	def __str__(self) -> str:
-		res = f'Packet with version {self.version} and type_id {self.type_id}\n'
-		for p in self.subpackets:
-			res += str(p)
-		return res
-
 	def getvalue(self) -> int:
-		print(self)
 		match self.type_id:
 			case 0:
 				return sum([p.getvalue() for p in self.subpackets])
@@ -120,15 +100,12 @@ class Packet:
 
 
 puzzle_input = open('input.txt').read()
-print(puzzle_input)
-packets = []
 binary_rep = bin(int(puzzle_input, 16)).lstrip('0b')
-print(f'binary_rep used to be {binary_rep, len(binary_rep)}')
 if len(binary_rep) % 4:
 	binary_rep = '0' * (4 - len(binary_rep) % 4) + binary_rep
+if puzzle_input[0] == '0':
+	binary_rep = '0' * 4 + binary_rep
 packet = Packet()
 ret = packet.parse_packet(binary_rep)
 print(f'Part1: {packet.get_versions()}')
-# print(str(packet))
-bigvalue = packet.getvalue()
-print(f'Part2: {bigvalue}')
+print(f'Part2: {packet.getvalue()}')
