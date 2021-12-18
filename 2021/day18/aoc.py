fec16a77fd6915665425537a4866d46437498d2b
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-18 17:32:41 +0100

    reeeeeeeeeeeeeeeeeeeeeeee

--- 2021/day18/aoc.py
+++ 2021/day18/aoc.py
@@ -1,4 +1,6 @@
 import math
+from copy import deepcopy
+from itertools import permutations
 
 
 class Pair:
@@ -50,9 +52,7 @@ class Pair:
 			elm.y += val
 
 	def giveright(self, val: int, s):
-		# print(f'giveright, self={self}, val={val}, s={s}')
 		if self.x == s:
-			# print(f'TRUE')
 			self.y -= -val
 		else:
 			elm = self.x
@@ -63,33 +63,16 @@ class Pair:
 	def dosplit(self) -> bool:
 		for i, item in enumerate([self.x, self.y]):
 			if isinstance(item, int) and item >= 10:
-				# print(f'item = {item}')
 				half = item / 2
+				print(f'Splitted {item}')
 				if i == 0:
 					self.x = Pair(item // 2, math.ceil(half))
 					self.x.parent = self
-					# print(f'SPLIT {item} into {self.x}')
-				# print(f'self.x={self.x}')
 				else:
 					self.y = Pair(item // 2, math.ceil(half))
 					self.y.parent = self
-					# print(f'SPLIT {item} into {self.y}')
-					# print(f'self.y={self.y}')
 				return True
 		return False
-
-	# def dosomething(self, depth: int = 0) -> bool:
-	# 	for i, item in enumerate([self.x, self.y]):
-	# 		if isinstance(item, Pair):
-	# 			# print(f'depth={depth}, item={item}')
-	# 			if depth == 3 and self.doexplode():
-	# 				return True
-	# 		else:
-	# 			if self.dosplit():
-	# 				return True
-	# 		if isinstance(item, Pair) and item.dosomething(depth + 1):
-	# 			return True
-	# 	return False
 
 	def first_explode(self, depth: int = 0) -> bool:
 		for i, item in enumerate([self.x, self.y]):
@@ -102,16 +85,16 @@ class Pair:
 
 	def then_split(self) -> bool:
 		for i, item in enumerate([self.x, self.y]):
-			if self.dosplit():
-				return True
 			if isinstance(item, Pair) and item.then_split():
+				return True
+			if self.dosplit():
 				return True
 		return False
 
 	def doexplode(self) -> bool:
 		for item in [self.x, self.y]:
 			if isinstance(item, Pair):
-				# print(f'Exploding {item}')
+				print(f'Exploding {item}')
 				left, right = item.x, item.y
 				p, s = self.parent, self
 				if item == self.x:
@@ -166,34 +149,44 @@ def parse_row(__row: str) -> Pair:
 	return pair
 
 
-def reduce(nbs: list[Pair]) -> int:
+def reduce(og: list[Pair]) -> int:
 	did_something = True
 	adds = 0
+	nbs = deepcopy(og)
 	while len(nbs) > 1 or did_something:
-		if adds == 4:
-			print(f'num={str(nbs[0])}')
 		did_something = False
-		if nbs[0].first_explode(0):
+		while nbs[0].first_explode(0) or nbs[0].then_split():
+			print(f'num={str(nbs[0])}')
 			did_something = True
-			continue
-		# print(f'lets explode')
-		if nbs[0].then_split():
-			did_something = True
-			continue
+			pass
 
 		if len(nbs) > 1:
-			print(f'...={str(nbs[0])}')
+			# Reduce nbs[1]
+			while nbs[1].first_explode(0) or nbs[1].then_split():
+				pass
 			nbs[0] = nbs[0] + nbs[1]
 			nbs.pop(1)
+			print(f'AFTER ADDING NEW ROW, NBS = {str(nbs[0])}')
 			adds += 1
 			did_something = True
 			# print(f'ADDED')
 			continue
 
-	print(f'{nbs[0]}')
+	# print(f'{nbs[0]}')
 	return nbs[0].get_magnitude()
+
+
+def part2(nbs: list[Pair]) -> int:
+	poss = permutations(nbs, 2)
+	answer = 0
+	for i, p in enumerate(poss):
+		tmp = reduce(list(p))
+		answer = max(answer, tmp)
+		# print(f'{p[0]} + {p[1]} = {tmp}')
+	return answer
 
 
 lines = open('input.txt').read().splitlines()
 rows = [parse_row(s) for s in lines]
 print(f'Part1: {reduce(rows)}')
+# print(f'Part2: {part2(rows)}')
