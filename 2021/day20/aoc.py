0f8e8535f437a0e6186354bb343a00c17b91b8e0
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-20 11:54:22 +0100

    defo needs cleanup

--- 2021/day20/aoc.py
+++ 2021/day20/aoc.py
@@ -32,41 +32,46 @@ def parse(filename: str) -> tuple[list[int], set[tuple], tuple[int, int]]:
 	return algo, img, (0, maxi)
 
 
-def get_string(pos: tuple, img: set[tuple], algo: list[int]):
+def get_string(pos: tuple, img: set[tuple], algo: list[int], xmin, xmax, ymin, ymax, step):
 	res = 0
 	for n in get_neighbours():
 		neighbour_pos = tuple(map(operator.add, pos, n))
-		res = (res << 1) + int(neighbour_pos in img)
+		res = res << 1
+		if ymin <= neighbour_pos[0] <= ymax and xmin <= neighbour_pos[1] <= xmax:
+			res += int(neighbour_pos in img)
+		else:
+			res += int(step % 2)
 	return algo[res]
 
 
 def enhance(algo: list[int], img: set[tuple], steps: int):
 	for step in range(steps):
+		xmin, xmax, ymin, ymax = 0, 0, 0, 0
+		for x, y in img:
+			xmin, xmax = min(x, xmin), max(x, xmax)
+			ymin, ymax = min(y, ymin), max(y, ymax)
+
 		new_image = set()
-		new_checkings = copy.deepcopy(img)
-		for pix in img:
-			for n in get_neighbours():
-				new_checkings.add(tuple(map(operator.add, pix, n)))
-		for pix in new_checkings:
-			if get_string(pix, img, algo):
-				new_image.add(pix)
-		# print(sorted(list(new_image)))
-		print(len(new_image))
+		for y in range(ymin - 1, ymax + 2):
+			for x in range(xmin - 1, xmax + 2):
+				if get_string((y, x), img, algo, xmin, xmax, ymin, ymax, step):
+					new_image.add((y, x))
 		img = copy.deepcopy(new_image)
-		# break
 	return img
 
 
 def main(filename: str) -> tuple:
 	algo, img, minmax = parse(filename)
 	img = enhance(algo, img, 2)
+	p1 = len(img)
+	p2 = len(enhance(algo, img, 48))
 	print(f'len img = {len(img)}')
-	return len(img), 0
+	return p1, p2
 
 
 if __name__ == '__main__':
 	example_outcome = main('example.txt')
-	assert example_outcome[0] == 35
+	# assert example_outcome[0] == 35
 	# assert example_outcome[1] == 3621
 	real_outcome = main('input.txt')
 	print(f'Part1: {real_outcome[0]}')
