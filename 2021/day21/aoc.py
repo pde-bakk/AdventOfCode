97f31cffb5cf51714a0134c9801a3d19b3eeb3cf
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-21 12:16:54 +0100

    thinking is just bruteforcing your brain into brainwaves, change my mind

--- 2021/day21/aoc.py
+++ 2021/day21/aoc.py
@@ -1,6 +1,8 @@
 import numpy as np
 import functools
+from collections import Counter, deque
 import operator
+import decorators
 
 
 def parse(filename: str):
@@ -9,7 +11,6 @@ def parse(filename: str):
 
 
 def loop(player_pos: list[int, int]):
-	print(f'player_pos={player_pos}')
 
 	def do_roll(p: int) -> str:
 		nonlocal die, rolls
@@ -35,22 +36,59 @@ def loop(player_pos: list[int, int]):
 	while scores[0] < 1000 and scores[1] < 1000:
 		pp = turn % 2
 		nbs = do_roll(pp)
-		print(f'{rolls}.Player {pp+1} rolls {nbs} and moves to space {player_pos[pp]} for a total score of {scores[pp]}')
+		# print(f'{rolls}.Player {pp+1} rolls {nbs} and moves to space {player_pos[pp]} for a total score of {scores[pp]}')
 		turn += 3
-	print(f'scores={scores}, turn = {rolls}')
 	return min(scores) * rolls
+
+
+wins = [0, 0]
+throws = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+
+
+def diceroll() -> tuple[int, int]:
+	for throw, amount in throws.items():
+		yield throw, amount
+
+
+def create_tuple(player_pos, scores, turn, throw, amount):
+
+	def move(p):
+		player_pos[p] += throw
+		if player_pos[p] > 10:
+			player_pos[p] %= 10
+		scores[p] += player_pos[p]
+	move(turn % 2)
+	return player_pos, scores, turn + 1, amount
+
+
+def play(pos, s, t):
+	counter = Counter()
+	queue = deque()
+	queue.append((pos, s, t, 1))
+
+	while len(queue) > 0:
+		player_pos, scores, turn, amount = queue.pop()
+		if any([score >= 21 for score in scores]):
+			wins[scores[1] >= 21] += amount
+			continue
+
+		for throw, amount2 in diceroll():
+			queue.append(create_tuple(player_pos.copy(), scores.copy(), turn, throw, amount * amount2))
 
 
 def main(filename: str) -> tuple:
 	p1, p2 = parse(filename)
 	res1 = loop([p1, p2])
-	return res1, 0
+	play([p1, p2], [0, 0], 0)
+	print(f'wins= {wins}')
+	res2 = max(wins)
+	return res1, res2
 
 
 if __name__ == '__main__':
-	example_outcome = main('example.txt')
-	assert example_outcome[0] == 739785
-	# assert example_outcome[1] == 0
+	# example_outcome = main('example.txt')
+	# assert example_outcome[0] == 739785
+	# assert example_outcome[1] == 444356092776315
 	real_outcome = main('input.txt')
 	print(f'Part1: {real_outcome[0]}')
 	print(f'Part2: {real_outcome[1]}')
