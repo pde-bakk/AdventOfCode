b17e6d5c4e3621fbd911a0ab658165a331880368
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-22 22:17:17 +0100

    desperation is starting to set in

--- 2021/day22/aoc.py
+++ 2021/day22/aoc.py
@@ -2,11 +2,12 @@ import numpy as np
 import functools
 from collections import Counter, deque
 import operator
+import math
 
 
 class Cube:
-	def __init__(self, arg=None, onoff='off'):
-		self.onoff = onoff
+	def __init__(self, arg=None, state=0):
+		self.state = state
 		self.x, self.y, self.z = (0, 0), (0, 0), (0, 0)
 		if isinstance(arg, str):
 			self.parse(arg)
@@ -25,7 +26,8 @@ class Cube:
 		return False
 
 	def parse(self, row: str):
-		self.onoff, coords = row.split(' ')
+		state, coords = row.split(' ')
+		self.state = bool(state == 'on')
 		coords = [s[2:] for s in coords.split(',')]
 		x2, y2, z2 = coords
 		self.x = tuple([int(x) for x in x2.split('..')])
@@ -36,43 +38,50 @@ class Cube:
 		for c in [self.x, self.y, self.z]:
 			yield c
 
+	def does_overlap(self, other) -> bool:
+		return self.x[1] >= other.x[0] and self.x[0] < other.x[1] and \
+				self.y[1] >= other.y[0] and self.y[0] < other.y[1] and \
+				self.z[1] >= other.z[0] and self.z[0] < other.z[1]
+
 	def get_intersection(self, other):
 		if not self.does_overlap(other):
 			return None
 		new_cube = [(max(a[0], b[0]), min(a[1], b[1])) for a, b in zip(self, other)]
-		# print(f'new_cube = {new_cube}')
-		intersection = Cube(new_cube, other.onoff)
-		return intersection
+		return Cube(new_cube, other.state)
 
 	def isvalid(self) -> bool:
-		return self.getvolume() > 0 and self.x[1] >= self.x[0] and self.y[1] >= self.y[0] and self.z[1] >= self.z[0]
+		return self.x[1] >= self.x[0] and self.y[1] >= self.y[0] and self.z[1] >= self.z[0]
 
 	def get_difference(self, other):
 		inter = self.get_intersection(other)
+		print(f'self={self}')
+		print(f'other={other}')
+		print(f'intersection={inter}\n')
 		if inter is None:
 			return None
-		subcubes = []
-		subcubes.append(Cube([self.x, (self.y[0], inter.y[0]), self.z], onoff=self.onoff))
-		subcubes.append(Cube([self.x, (inter.y[1], self.y[1]), self.z], onoff=self.onoff))
-		subcubes.append(Cube([(self.x[0], inter.x[0]), inter.y, self.z], onoff=self.onoff))
-		subcubes.append(Cube([(inter.x[1], self.x[1]), inter.y, self.z], onoff=self.onoff))
-		subcubes.append(Cube([inter.x, inter.y, (self.z[0], inter.z[0])], onoff=self.onoff))
-		subcubes.append(Cube([inter.x, inter.y, (inter.z[1], self.z[1])], onoff=self.onoff))
+		subcubes = [
+			Cube([self.x, (self.y[0], inter.y[0] - 1), self.z], state=self.state),
+			Cube([self.x, (inter.y[1] + 1, self.y[1]), self.z], state=self.state),
+			Cube([(self.x[0], inter.x[0] - 1), inter.y, self.z], state=self.state),
+			Cube([(inter.x[1] + 1, self.x[1]), inter.y, self.z], state=self.state),
+			Cube([inter.x, inter.y, (self.z[0], inter.z[0] - 1)], state=self.state),
+			Cube([inter.x, inter.y, (inter.z[1] + 1, self.z[1])], state=self.state)
+		]
+
+		for i, c in enumerate(subcubes):
+			print(f'subcube[{i}]={c}')
 		return [c for c in subcubes if c.isvalid()]
 
 	def __repr__(self):
-		return f'Cube: {self.x}, {self.y}, {self.z} <= {self.onoff}, volume={self.getvolume()}'
+		return f'Cube: {self.x}, {self.y}, {self.z} <= {self.state}, volume = {self.getvolume()}'
 
 	def values(self):
 		return [self.x[0], self.x[1], self.y[0], self.y[1], self.z[0], self.z[1]]
 
 	def getvolume(self):
+		# if not self.isvalid():
+		# 	return -1
 		return abs(self.x[1] - self.x[0] + 1) * abs(self.y[1] - self.y[0] + 1) * abs(self.z[1] - self.z[0] + 1)
-
-	def does_overlap(self, other) -> bool:
-		return self.x[1] > other.x[0] and self.x[0] < other.x[1] and \
-				self.y[1] > other.y[0] and self.y[0] < other.y[1] and \
-				self.z[1] > other.z[0] and self.z[0] < other.z[1]
 
 
 def parse(filename: str):
@@ -89,27 +98,26 @@ def part1(cubes: list[Cube]):
 			for y in range(cube.y[0], cube.y[1] + 1):
 				for x in range(cube.x[0], cube.x[1] + 1):
 					tup = (x, y, z)
-					if cube.onoff == 'on':
+					if cube.state:
 						ons.add(tup)
 					elif tup in ons:
 						ons.remove(tup)
 	return len(ons)
 
 
-def part2(lines: list[Cube]) -> int:
+def part2(cubes: list[Cube]) -> int:
 	all_cubes = []
-	for line in lines:
+	for existing_cube in cubes:
 		new_cubes = []
-		print(f'line={line}')
 		for cube in all_cubes:
-			if cube.does_overlap(line):
-				result = cube.get_difference(line)
+			if cube.does_overlap(existing_cube):
+				result = cube.get_difference(existing_cube)
 				if result is not None:
+					print(f'diff has len={len(result)}')
 					new_cubes.extend(result)
-		if line.onoff == 'on':
-			new_cubes.append(line)
-		all_cubes.extend(new_cubes)
-		new_cubes.clear()
+		if existing_cube.state:
+			new_cubes.append(existing_cube)
+		all_cubes = new_cubes
 	return sum([c.getvolume() for c in all_cubes])
 
 
@@ -124,7 +132,7 @@ def main(filename: str) -> tuple:
 
 if __name__ == '__main__':
 	example_outcome = main('example.txt')
-	assert example_outcome[0] == 474140
+	# assert example_outcome[0] == 474140
 	assert example_outcome[1] == 2758514936282235
 	real_outcome = main('input.txt')
 	print(f'Part1: {real_outcome[0]}')
