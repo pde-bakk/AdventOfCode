42a7252f672a8bfb017da61a994d9511c2a5779c
Peer de Bakker <pde-bakk@student.codam.nl>
2021-12-22 16:10:01 +0100

    gotta continue later

--- 2021/day22/aoc.py
+++ 2021/day22/aoc.py
@@ -2,13 +2,27 @@ import numpy as np
 import functools
 from collections import Counter, deque
 import operator
-#import decorators
 
 
 class Cube:
-	def __init__(self):
-		self.onoff = 'off'
+	def __init__(self, arg=None, onoff='off'):
+		self.onoff = onoff
 		self.x, self.y, self.z = (0, 0), (0, 0), (0, 0)
+		if isinstance(arg, str):
+			self.parse(arg)
+		elif isinstance(arg, list):
+			self.x, self.y, self.z = arg
+
+	def __eq__(self, other):
+		return self.x == other.x and self.y == other.y and self.z == other.z
+
+	def __lt__(self, other):
+		for c, c2 in zip([self.x, self.y, self.z], [other.x, other.y, other.z]):
+			if c < c2:
+				return True
+			elif c > c2:
+				return False
+		return False
 
 	def parse(self, row: str):
 		self.onoff, coords = row.split(' ')
@@ -18,6 +32,17 @@ class Cube:
 		self.y = tuple([int(x) for x in y2.split('..')])
 		self.z = tuple([int(x) for x in z2.split('..')])
 
+	def __iter__(self):
+		for c in [self.x, self.y, self.z]:
+			yield c
+
+	def get_intersection(self, other):
+		if not self.does_overlap(other):
+			return None
+		new_cube = [(max(a[0], b[0]), min(a[1], b[1])) for a, b in zip(self, other)]
+		print(f'new_cube = {new_cube}')
+		intersection = Cube(new_cube, other.onoff)
+
 	def values(self):
 		return [self.x[0], self.x[1], self.y[0], self.y[1], self.z[0], self.z[1]]
 
@@ -26,25 +51,21 @@ class Cube:
 
 	def does_overlap(self, other) -> bool:
 		return self.x[1] > other.x[0] and self.x[0] < other.x[1] and \
-			self.y[1] > other.y[0] and self.y[0] < other.y[1] and \
-			self.z[1] > other.z[0] and self.z[0] < other.z[1]
+				self.y[1] > other.y[0] and self.y[0] < other.y[1] and \
+				self.z[1] > other.z[0] and self.z[0] < other.z[1]
 
-	def overlap(self, other):
-		if self.x[1] > other.x[0] and self.x[0] < other.x[1] and \
-		self.y[1] > other.y[0] and self.y[0] < other.y[1] and \
-		self.z[1] > other.z[0] and self.z[0] < other.z[1]:
-				x
-			pass
-		raise ArithmeticError
+	# def overlap(self, other):
+	# 	if self.x[1] > other.x[0] and self.x[0] < other.x[1] and \
+	# 			self.y[1] > other.y[0] and self.y[0] < other.y[1] and \
+	# 			self.z[1] > other.z[0] and self.z[0] < other.z[1]:
+	# 		return T
+	# 		pass
+	# 	raise ArithmeticError
+
 
 def parse(filename: str):
 	rows = open(filename).read().splitlines()
-	parsed = []
-	for row in rows:
-		cube = Cube()
-		cube.parse(row)
-		parsed.append(cube)
-	return parsed
+	return [Cube(row) for row in rows]
 
 
 def part1(cubes: list[Cube]):
@@ -63,21 +84,30 @@ def part1(cubes: list[Cube]):
 	return len(ons)
 
 
-def part2(cubes: list[Cube]) -> int:
-	for cube in cubes:
-
+def part2(lines: list[Cube]) -> int:
+	all_cubes = []
+	for line in lines:
+		new_cubes = []
+		for cube in all_cubes:
+			if cube.does_overlap(line):
+				overlap = cube.get_intersection()
+		all_cubes.extend(new_cubes)
+		new_cubes.clear()
+		pass
+	return 0
 
 
 def main(filename: str) -> tuple:
 	rows = parse(filename)
 	on_beacons = part1(rows)
 	print(f'on_beacons={on_beacons}')
+	part2(rows)
 	return on_beacons, 0
 
 
 if __name__ == '__main__':
 	example_outcome = main('example.txt')
-	assert example_outcome[0] == 47140
+	assert example_outcome[0] == 474140
 	assert example_outcome[1] == 2758514936282235
 	real_outcome = main('input.txt')
 	print(f'Part1: {real_outcome[0]}')
