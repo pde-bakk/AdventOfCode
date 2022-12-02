bb0a00f1009d67814743af18557b73693c86e96c
pde-bakk <pde-bakk@student.codam.nl>
2022-12-02 12:18:47 +0100

    big cleanup

--- 2022/day02/day02.py
+++ 2022/day02/day02.py
@@ -1,34 +1,29 @@
+SCORES = {
+	'A X': 4,
+	'A Y': 8,
+	'A Z': 3,
+	'B X': 1,
+	'B Y': 5,
+	'B Z': 9,
+	'C X': 7,
+	'C Y': 2,
+	'C Z': 6,
+}
+
+CHOICES = {
+	'A X': 'Z',
+	'A Y': 'X',
+	'A Z': 'Y',
+	'B X': 'X',
+	'B Y': 'Y',
+	'B Z': 'Z',
+	'C X': 'Y',
+	'C Y': 'Z',
+	'C Z': 'X',
+}
+
 with open('input.txt', 'r') as f:
 	lines = f.read().splitlines()
-my_score, elf_score = [], []
-for line in lines:
-	elf, you = list(map(ord, line.split()))
-	print(f'{chr(elf)} vs {chr(you)}')
-	if you == ord('Y'):
-		you = elf
-	elif you == ord('X'):
-		if elf == ord('A'):
-			you = ord('C')
-		else:
-			you = elf - 1
-		# you = elf - 1 if elf != ord('A') else ord('C')
-	elif you == ord('Z'):
-		if elf == ord('C'):
-			you = ord('A')
-		else:
-			you = elf + 1
-		# you = elf + 1 if elf != ord('C') else ord('A')
-	print(f'you chose {chr(you)}')
-	my_tmp_score = you - ord('A') + 1
-	elf_tmp_score = elf - ord('A') + 1
-	if you == elf:
-		my_tmp_score += 3
-		elf_tmp_score += 3
-	elif you == elf + 1 or you == ord('A') and elf == ord('C'):
-		my_tmp_score += 6
-	else:
-		elf_tmp_score += 6
-	my_score.append(my_tmp_score)
-	elf_score.append(elf_tmp_score)
-	print(f'Elf score: {elf_tmp_score} vs My score: {my_tmp_score}')
-print(sum(my_score))
+print(f'Part 1: {sum([SCORES[line] for line in lines])}')
+lines = [line[:-1] + CHOICES[line] for line in lines]
+print(f'Part 2: {sum([SCORES[line] for line in lines])}')
