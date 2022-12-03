5219351ecc985c101ac644a025b24cab807cbc61
pde-bakk <pde-bakk@student.codam.nl>
2022-12-03 18:17:52 +0100

    107 chars

--- 2022/day03/codegolf_p1.py
+++ 2022/day03/codegolf_p1.py
@@ -1,2 +1,2 @@
 import string
-print(sum([string.ascii_letters.index(*(set(i[:l])&set(i[l:])))for i in open(0)if(l:=len(i)//2)]))
+print(sum(string.ascii_letters.index(*({*i[:l]}&{*i[l:]}))for i in open(0)if(l:=len(i)//2)))
