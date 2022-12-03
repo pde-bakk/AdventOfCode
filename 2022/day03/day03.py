e2e0559c2cc69a6215c61a882b69c29549b205e3
pde-bakk <pde-bakk@student.codam.nl>
2022-12-03 11:53:19 +0100

    easy

--- 2022/day03/day03.py
+++ 2022/day03/day03.py
@@ -2,7 +2,7 @@ with open('input.txt', 'r') as f:
 	lines = f.read().splitlines()
 
 total = 0
-for line in lines:
+for i, line in enumerate(lines):
 	first, second = line[:len(line)//2], line[len(line)//2:]
 	common = set(first) & set(second)
 	for item in common:
@@ -10,6 +10,22 @@ for line in lines:
 			value = ord(item) - ord('a') + 1
 		else:
 			value = ord(item) - ord('A') + 27
-		print(value)
 		total += value
+print(total)
+
+elfs = []
+total = 0
+for i, line in enumerate(lines):
+	if i % 3 == 0:
+		elfs.clear()
+	elfs.append(set(line))
+
+	if i % 3 == 2:
+		common = elfs[0] & elfs[1] & elfs[2]
+		for item in common:
+			if item.islower():
+				value = ord(item) - ord('a') + 1
+			else:
+				value = ord(item) - ord('A') + 27
+			total += value
 print(total)
