a4261407ea8a587340a10909027c3c0a386b1562
pde-bakk <pde-bakk@student.codam.nl>
2022-12-04 13:02:44 +0100

    rewrote day 4 to use regex, since I want to be more comfortable with regex

--- 2022/day04/day04.py
+++ 2022/day04/day04.py
@@ -1,16 +1,16 @@
+import re
+
 with open('input.txt', 'r') as f:
 	lines = f.read().splitlines()
 
 part_1 = part_2 = 0
 for line in lines:
-	elfs = line.split(',')
-	for i in range(2):
-		A = [*map(int, elfs[i].split('-'))]
-		A[-1] += 1
-		elfs[i] = set(range(*A))
-	if elfs[0] <= elfs[1] or elfs[1] <= elfs[0]:
+	ints = [*map(int, re.findall(r'\d+', line))]
+	sets = set(range(ints[0], ints[1] + 1)), set(range(ints[2], ints[3] + 1))
+	if sets[0].issubset(sets[1]) or sets[0].issuperset(sets[1]):
 		part_1 += 1
-	if elfs[0] & elfs[1]:
+	if sets[0] & sets[1]:
 		part_2 += 1
+
 print(f'Part 1: {part_1}')
 print(f'Part 2: {part_2}')
