b6ef8488b6aff526a8054b8f4c5fea41f31bc27d
pde-bakk <pde-bakk@student.codam.nl>
2022-12-05 14:21:45 +0100

    tiny cleanup

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -1,22 +1,31 @@
 import re
+from copy import deepcopy
+
+
+def simulate_instructions(containers: list, part: int) -> str:
+	for line in instructions.splitlines():
+		ints = [*map(int, re.findall(r'\d+', line))]
+		if part == 1:
+			for i in range(ints[0]):
+				top = containers[ints[1] - 1].pop()
+				containers[ints[2] - 1].append(top)
+		else:
+			for i in range(ints[0], 0, -1):
+				top = containers[ints[1] - 1].pop(-i)
+				containers[ints[2] - 1].append(top)
+	return ''.join(container[-1] for container in containers)
+
 
 with open('input.txt', 'r') as f:
 	lines = f.read()
-C, I = lines.split('\n\n')
+C, instructions = lines.split('\n\n')
 C = C.splitlines()[::-1]
 idxs = {i: int(c) for i, c in enumerate(C[0]) if c.isdigit()}
-containers = [[] for _ in idxs.keys()]
-for line in C[1:]:
+conts = [[] for _ in idxs.keys()]
+for line_c in C[1:]:
 	for k, v in idxs.items():
-		if k < len(line) and line[k].isupper():
-			containers[v - 1].append(line[k])
-print(containers)
+		if k < len(line_c) and line_c[k].isupper():
+			conts[v - 1].append(line_c[k])
 
-for line in I.splitlines():
-	ints = [*map(int, re.findall(r'\d+', line))]
-	print(ints)
-	for i in range(ints[0], 0, -1):
-		top = containers[ints[1] - 1].pop(-i)
-		containers[ints[2] - 1].append(top)
-for c in containers:
-	print(c[-1], end='')
+print('Part 1:' + simulate_instructions(deepcopy(conts), part=1))
+print('Part 2:' + simulate_instructions(deepcopy(conts), part=2))
