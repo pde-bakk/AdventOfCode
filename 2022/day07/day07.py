daafb6086ba5bb4660a568c0f09092eeb0f90a9d
pde-bakk <pde-bakk@student.codam.nl>
2022-12-07 13:09:16 +0100

    decided to rework it using pythons pattern matching

--- 2022/day07/day07.py
+++ 2022/day07/day07.py
@@ -1,47 +1,20 @@
 with open('input.txt', 'r') as f:
 	lines = f.read().splitlines()
 
-cwd = ''
-directories = {
-	'/': 0
-}
+cwd = []
+directories = {}
 for line in lines:
-	tokens = line.split()
-	if tokens[0] == '$':
-		if tokens[1] == 'cd':
-			path = tokens[-1]
-			if path == '..':
-				last_slash_idx = cwd.rfind('/')
-				cwd = cwd[:last_slash_idx]
-				if not cwd:
-					cwd = '/' + cwd
-			else:
-				if cwd and cwd[-1] != '/':
-					cwd += '/'
-				cwd += path
-	else:
-		if tokens[0] == 'dir':
-			continue
-		size, filename = int(tokens[0]), tokens[1]
-		subpath = cwd
-		while subpath:
-			directories[subpath] = directories.get(subpath, 0) + size
-			new_subpath = subpath[:subpath.rfind('/')]
-			if subpath != '/' and new_subpath == '':
-				subpath = '/'
-			else:
-				subpath = new_subpath
+	match line.split():
+		case '$', 'cd', '..':
+			cwd.pop()
+		case '$', 'cd', path:
+			cwd += [path]
+		case ['$', 'ls'] | ['dir', _]:
+			pass
+		case filesize, filename:
+			for i in range(len(cwd), 0, -1):
+				folder = '_'.join(cwd[:i])
+				directories[folder] = directories.get(folder, 0) + int(filesize)
 
-part_1 = part_2 = 0
-for directory, size in directories.items():
-	if size <= 100000 and directory != '/':
-		part_1 += size
-print(f'Part_1: {part_1}')
-
-
-TOTAL_SPACE, WANTED_UNUSED_SPACE = 70_000_000, 30_000_000
-USED_SPACE, UNUSED_SPACE = directories['/'], TOTAL_SPACE - directories['/']
-for size in sorted(directories.values()):
-	if UNUSED_SPACE + size >= WANTED_UNUSED_SPACE:
-		print(f'Part 2: {size}')
-		break
+print('Part_1:', sum(v for k, v in directories.items() if v <= 100_000))
+print('Part_2:', min(v for k, v in directories.items() if v >= directories['/'] - 40_000_000))
