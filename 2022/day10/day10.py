816396f04faeb44cec1e17685e0d37311e96402d
pde-bakk <pde-bakk@student.codam.nl>
2022-12-10 14:40:15 +0100

    CRT is dope

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -8,21 +8,30 @@ def update_cycle():
 	cycles[cycle] = X
 
 
+def draw_on_crt(c):
+	char = '#' if abs(X - (c % 40)) <= 1 else '.'
+	crt[c // 40][c % 40] = char
+
+
 X = 1
 cycle = 1
 cycles = {}
+crt = [['.'] * 40 for _ in range(6)]
 for line in lines:
 	tokens = line.split()
 	match tokens[0]:
 		case 'noop':
+			draw_on_crt(cycle - 1)
 			update_cycle()
 		case 'addx':
+			draw_on_crt(cycle - 1)
 			update_cycle()
+			draw_on_crt(cycle - 1)
 			X += int(tokens[1])
-			# print(f'Cycle is {cycle},{tokens[1]=} {X=}')
 			update_cycle()
 
-for c in [20, 60, 100, 140, 180, 220]:
-	print(f'During cycle {c}, {X=}, {c * X =}')
 
 print(f'Part 1:', sum(cycles[c] * c for c in [20, 60, 100, 140, 180, 220]))
+print(f'Part 2:')
+for row in crt:
+	print(''.join(row))
