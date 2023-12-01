96ebb3baa35d49e052361b39132ce2e0f1abfc6a
pde-bakk <pde-bakk@student.codam.nl>
2023-12-01 13:47:09 +0100

    down to 221 bytes for part 2, still no good

--- 2023/day01/part2_golf.py
+++ 2023/day01/part2_golf.py
@@ -1,7 +1,8 @@
 L='one two three four five six seven eight nine'.split()
 S=0
+e=enumerate
 for r in open(0):
-	for i,l in enumerate(L):r=r.replace(l,l[0]+str(i+1)+l[2:])
-	d=[(i,int(c))for i,c in enumerate(r)if'/'<c<':']
+	for i,l in e(L):r=r.replace(l,l[0]+str(i+1)+l[2:])
+	d=[(i,int(c))for i,c in e(r)if'/'<c<':']
 	S+=min(d)[1]*10+max(d)[1]
 print(S)
