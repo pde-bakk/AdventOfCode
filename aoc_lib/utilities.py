1edb9b057339796ad7f0c912631b97cd552a39eb
pde-bakk <pde-bakk@student.codam.nl>
2023-12-04 18:57:32 +0100

    now with template to copy into new folders, and automatically retrieving the example input too

---
+++ aoc_lib/utilities.py
@@ -0,0 +1,50 @@
+import re
+
+
+def lmap(func, *it) -> list:
+	return list(map(func, *it))
+
+def ints(s: str) -> list[int]:
+	return lmap(int, re.findall(r'-?\d+', s))
+def pos_ints_only(s: str) -> list[int]:
+	return lmap(int, re.findall(r'\d+', s))
+def floats(s: str) -> list[float]:
+	return lmap(float, re.findall(r'-?\d+(?:\.\d+)?', s))
+def positive_floats(s: str) -> list[float]:
+	return lmap(float, re.findall(r'\d+(?:\.\d+)?', s))
+def words(s: str) -> list[str]:
+	return re.findall(r'[a-zA-Z]+', s)
+
+
+def get_directions(diagonal: bool=False) -> list[tuple[int, int]]:
+	directions = [
+		(0, 1), # South
+		(0, -1), # North
+		(1, 0), # East
+		(-1, 0), # West
+	]
+	if diagonal:
+		directions += [
+			(1, 1), # South East
+			(1, -1), # South West
+			(-1, 1), # North East
+			(-1, -1) # North West
+		]
+	return directions
+
+def yield_directions(diagonal: bool=False) -> tuple[int, int]:
+	for direction in get_directions(diagonal=diagonal):
+		yield direction
+
+def get_neighbours(y: int, x: int, diagonal: bool=False) -> list[tuple[int, int]]:
+	return [(y + d[0], x + d[1]) for d in get_directions(diagonal=diagonal)]
+
+def yield_neighbours(y: int, x: int, diagonal: bool=False) -> tuple[int, int]:
+	for n in get_neighbours(y, x, diagonal):
+		yield n
+
+def distance(pos1: tuple[int, int], pos2: tuple[int, int]) -> float:
+	return ((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
+
+def manhattan_distance(pos1: tuple[int, int], pos2: tuple[int, int]) -> int:
+	return (pos1[0] - pos2[0]) + (pos1[1] - pos2[1])
---
+++ template.py
@@ -0,0 +1,17 @@
+import sys
+import math
+sys.path.append('../..')
+from aoc_lib.get_input import get_input_file, get_example_file
+from aoc_lib.utilities import *
+
+
+def aoc(lines: list[str], prefix: str) -> None:
+	part1 = 0
+	part2 = 0
+	print(f'{prefix} part 1: {part1}')
+	print(f'{prefix} part 2: {part2}')
+
+
+if __name__ == '__main__':
+	aoc(get_example_file(), 'Example')
+	aoc(get_input_file(), 'Solution')
