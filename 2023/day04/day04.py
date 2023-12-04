799f222d6e24629af3ffd5ee0431a223ab50a294
pde-bakk <pde-bakk@student.codam.nl>
2023-12-04 12:15:35 +0100

    thats a lotta scratchcards

--- 2023/day04/day04.py
+++ 2023/day04/day04.py
@@ -8,7 +8,8 @@ lines = get_input_file()
 # lines = open('sample.txt').read().splitlines()
 print(lines)
 part1 = 0
-for line in lines:
+d = {i: 1 for i in range(len(lines))}
+for i, line in enumerate(lines):
 	_, tickets = line.split(':')
 	a, b = tickets.split('|')
 	a = list(map(int, a.split()))
@@ -17,3 +18,13 @@ for line in lines:
 	if winning_nbs:
 		part1 += 2 ** (len(winning_nbs) - 1)
 print(part1)
+part2 = 0
+for i, line in enumerate(lines):
+	_, tickets = line.split(':')
+	a, b = tickets.split('|')
+	a = list(map(int, a.split()))
+	b = list(map(int, b.split()))
+	winning_nbs = [x for x in b if x in a]
+	for i2 in range(i+1, i + 1 + len(winning_nbs)):
+		d[i2] += 1 * d[i]
+print(sum(d.values()))
