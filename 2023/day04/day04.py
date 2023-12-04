ab78e170e636d35cde4d4ba241e6d8eb44e4863d
pde-bakk <pde-bakk@student.codam.nl>
2023-12-04 12:27:33 +0100

    more concise

--- 2023/day04/day04.py
+++ 2023/day04/day04.py
@@ -5,26 +5,16 @@ from aoc_lib.get_input import get_input_file
 from aoc_lib.utilities import *
 
 lines = get_input_file()
-# lines = open('sample.txt').read().splitlines()
-print(lines)
 part1 = 0
+part2 = 0
 d = {i: 1 for i in range(len(lines))}
 for i, line in enumerate(lines):
 	_, tickets = line.split(':')
 	a, b = tickets.split('|')
-	a = list(map(int, a.split()))
-	b = list(map(int, b.split()))
-	winning_nbs = [x for x in b if x in a]
+	winning_nbs = len(set(a.split()) & set(b.split()))
 	if winning_nbs:
-		part1 += 2 ** (len(winning_nbs) - 1)
-print(part1)
-part2 = 0
-for i, line in enumerate(lines):
-	_, tickets = line.split(':')
-	a, b = tickets.split('|')
-	a = list(map(int, a.split()))
-	b = list(map(int, b.split()))
-	winning_nbs = [x for x in b if x in a]
-	for i2 in range(i+1, i + 1 + len(winning_nbs)):
+		part1 += 2 ** (winning_nbs - 1)
+	for i2 in range(i+1, i + 1 + winning_nbs):
 		d[i2] += 1 * d[i]
-print(sum(d.values()))
+print(f'Part 1: {part1}')
+print(f'Part 2: {sum(d.values())}')
