a919a68d27d2d083e784e1c51bffd25e84c8747a
pde-bakk <pde-bakk@student.codam.nl>
2023-12-07 18:07:30 +0100

    realized I didnt actually need functools.key_to_cmp

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -1,5 +1,7 @@
-import functools
 import sys
+
+sys.path.append('../..')
+from aoc_lib.utilities import *
 
 sys.path.append('../..')
 from aoc_lib.get_input import get_input_file, get_example_file
@@ -12,40 +14,33 @@ def get_score(hand: str):
 	return [counts.count(i) for i in [5, 4, 3, 2]]
 
 
-def compare(card_a: tuple[str, str, int], card_b: tuple[str, str, int]):
-	hand_a, orig_a, _ = card_a
-	hand_b, orig_b, _ = card_b
+def camper(card: tuple[str, str, int]):
+	hand, og_hand, bid = card
 	card_strengths = ['23456789TJQKA', 'J23456789TQKA'][p2_flag]
-	for a, b in zip(get_score(hand_a), get_score(hand_b)):
-		if a != b:
-			return [-1, 1][a > b]
-	for a, b in zip(orig_a, orig_b):
-		if a != b:
-			return [-1, 1][card_strengths.index(a) > card_strengths.index(b)]
-	return 0
-
-
-def make_best_joker_hand(hand: str) -> str:
-	card_strengths = 'J23456789TQKA'
-	if 'J' not in hand:
-		return hand
-	arr = [(get_score(new_hand), new_hand) for replacement in card_strengths[1:] if (new_hand := hand.replace('J', replacement))]
-	return max(arr)[1]
+	return get_score(hand) + lmap(card_strengths.index, og_hand)
 
 
 def aoc(lines: list[str], prefix: str) -> None:
 	def calculate_winnings(x) -> int:
 		return sum([bid * idx for idx, (hand, og_hand, bid) in enumerate(x, start=1)])
+
+	def make_best_joker_hand(hand: str) -> str:
+		card_strengths = 'J23456789TQKA'
+		if 'J' not in hand:
+			return hand
+		arr = [(get_score(new_hand), new_hand) for replacement in card_strengths[1:] if (new_hand := hand.replace('J', replacement))]
+		return max(arr)[1]
+
 	global p2_flag
 	p2_flag = False
 	cards = [(hand, hand, int(bid)) for hand, bid in map(str.split, lines)]
-	cards.sort(key=functools.cmp_to_key(compare))
+	cards.sort(key=camper)
 	print(f'{prefix} part 1: {calculate_winnings(cards)}')
 
 	p2_flag = True
 	for i, (hand, hand, bid) in enumerate(cards):
 		cards[i] = make_best_joker_hand(hand), hand, bid
-	cards.sort(key=functools.cmp_to_key(compare))
+	cards.sort(key=camper)
 	print(f'{prefix} part 2: {calculate_winnings(cards)}')
 
 
