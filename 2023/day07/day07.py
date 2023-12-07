bd0a36a80e8e629b7abaf8c0f0b49644c0143287
pde-bakk <pde-bakk@student.codam.nl>
2023-12-07 17:46:41 +0100

    cleaned up after myself like a toddler

--- 2023/day07/day07.py
+++ 2023/day07/day07.py
@@ -1,85 +1,52 @@
 import functools
 import sys
-import math
 
 sys.path.append('../..')
 from aoc_lib.get_input import get_input_file, get_example_file
-from aoc_lib.utilities import *
-from aoc_lib.extra_input_utils import split_on_double_newlines_instead
 
 p2_flag = False
+
+
+def get_score(hand: str):
+	counts = [hand.count(c) for c in hand]
+	return [counts.count(i) for i in [5, 4, 3, 2]]
 
 
 def compare(card_a: tuple[str, str, int], card_b: tuple[str, str, int]):
 	hand_a, orig_a, _ = card_a
 	hand_b, orig_b, _ = card_b
-	card_strengths = '23456789TJQKA'
-	if p2_flag:
-		card_strengths = 'J23456789TQKA'
-	counts_a = [hand_a.count(c) for c in hand_a]
-	counts_b = [hand_b.count(c) for c in hand_b]
-	if counts_a.count(5) != counts_b.count(5):
-		print(f'5. {hand_a} > {hand_b}')
-		return [-1, 1][counts_a.count(5) > counts_b.count(5)]
-	if counts_a.count(4) != counts_b.count(4):
-		print(f'4. {hand_a} > {hand_b}')
-		return [-1, 1][counts_a.count(4) > counts_b.count(4)]
-	if int(3 in counts_a and 2 in counts_a) != int(3 in counts_b and 2 in counts_b):
-		print(f'FH. {hand_a} > {hand_b}')
-		return [-1, 1][int(3 in counts_a and 2 in counts_a) > int(3 in counts_b and 2 in counts_b)]
-	if counts_a.count(3) != counts_b.count(3):
-		print(f'3. {hand_a} > {hand_b}, {counts_a.count(3)} vs {counts_b.count(3)}')
-		return [-1, 1][counts_a.count(3) > counts_b.count(3)]
-	if counts_a.count(2) != counts_b.count(2):  # Maybe wrong
-		print(f'2. {hand_a} > {hand_b}')
-		return [-1, 1][counts_a.count(2) > counts_b.count(2)]
+	card_strengths = ['23456789TJQKA', 'J23456789TQKA'][p2_flag]
+	for a, b in zip(get_score(hand_a), get_score(hand_b)):
+		if a != b:
+			return [-1, 1][a > b]
 	for a, b in zip(orig_a, orig_b):
 		if a != b:
-			print(f'idx. {hand_a} {"<>"[card_strengths.index(a) > card_strengths.index(b)]} {hand_b}, cus {card_strengths.index(a)} vs {card_strengths.index(b)}')
-			print(f'{orig_a=}, {orig_b=}, {a,b=}')
 			return [-1, 1][card_strengths.index(a) > card_strengths.index(b)]
-	print(f'SHOULDNT HAPPEN. {hand_a} == {hand_b}')
 	return 0
-
-
-def get_scores(hand: str):
-	counts = [hand.count(c) for c in hand]
-	return [counts.count(i) for i in [5, 4, 3, 2]]
 
 
 def make_best_joker_hand(hand: str) -> str:
 	card_strengths = 'J23456789TQKA'
 	if 'J' not in hand:
 		return hand
-	arr = []
-	for replacement in card_strengths[1:]:
-		new_hand = hand.replace('J', replacement)
-		arr.append((get_scores(new_hand), new_hand))
-	best = max(arr)
-	print(f'For {hand=}, joker is best used to make the hand {best}')
-	return best[1]
+	arr = [(get_score(new_hand), new_hand) for replacement in card_strengths[1:] if (new_hand := hand.replace('J', replacement))]
+	return max(arr)[1]
 
 
 def aoc(lines: list[str], prefix: str) -> None:
-	part1 = 0
-	part2 = 0
-	cards = []
-	for hand, bid in map(str.split, lines):
-		bid = int(bid)
-		print(hand, bid)
-		cards.append((hand, hand, bid))
-	cards.sort(key=functools.cmp_to_key(compare))
-	print(cards)
-	part1 = sum([bid * idx for idx, (hand, og_hand, bid) in enumerate(cards, start=1)])
-	print(f'{prefix} part 1: {part1}')
+	def calculate_winnings(x) -> int:
+		return sum([bid * idx for idx, (hand, og_hand, bid) in enumerate(x, start=1)])
 	global p2_flag
+	p2_flag = False
+	cards = [(hand, hand, int(bid)) for hand, bid in map(str.split, lines)]
+	cards.sort(key=functools.cmp_to_key(compare))
+	print(f'{prefix} part 1: {calculate_winnings(cards)}')
+
 	p2_flag = True
 	for i, (hand, hand, bid) in enumerate(cards):
 		cards[i] = make_best_joker_hand(hand), hand, bid
 	cards.sort(key=functools.cmp_to_key(compare))
-	print(cards)
-	part2 = sum([bid * idx for idx, (hand, _, bid) in enumerate(cards, start=1)])
-	print(f'{prefix} part 2: {part2}')
+	print(f'{prefix} part 2: {calculate_winnings(cards)}')
 
 
 if __name__ == '__main__':
