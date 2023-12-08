84ff65091af496365dace8515bfb4c4429d7eaba
pde-bakk <pde-bakk@student.codam.nl>
2023-12-08 11:39:38 +0100

    created replace_all() method to return a string where all of the given chars are replaced by a another char or nothing if not given

--- aoc_lib/utilities.py
+++ aoc_lib/utilities.py
@@ -62,3 +62,9 @@ def distance(pos1: tuple[int, int], pos2: tuple[int, int]) -> float:
 
 def manhattan_distance(pos1: tuple[int, int], pos2: tuple[int, int]) -> int:
 	return (pos1[0] - pos2[0]) + (pos1[1] - pos2[1])
+
+
+def replace_all(s: str, chars_to_replace: str, replacement: str = '') -> str:
+	for c in chars_to_replace:
+		s = s.replace(c, replacement)
+	return s
