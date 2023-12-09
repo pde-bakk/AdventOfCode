a344f67265e4308c2bd5cbec49280476c9107522
pde-bakk <pde-bakk@student.codam.nl>
2023-12-09 12:57:46 +0100

    day 9 done in the nick of time

---
+++ 2023/day09/day09.py
@@ -0,0 +1,33 @@
+import sys
+import math
+sys.path.append('../..')
+from aoc_lib.get_input import get_input_file, get_example_file
+from aoc_lib.utilities import *
+from aoc_lib.extra_input_utils import split_on_double_newlines_instead
+
+
+def aoc(lines: list[str], prefix: str) -> None:
+	part1 = 0
+	part2 = 0
+	lines = [lmap(int, line.split()) for line in lines]
+	for line in lines:
+		history = [line]
+		while any(i != 0 for i in history[-1]):
+			diffs = [b - a for a, b in zip(history[-1], history[-1][1:])]
+			history.append(diffs)
+		history = history[::-1]
+		for i, his in enumerate(history):
+			if i == 0:
+				history[i] = [0] + his + [0]
+			else:
+				history[i] = [his[0] - history[i - 1][0]] + his + [history[i-1][-1] + his[-1]]
+			if i == len(history) - 1:
+				part1 += history[i][-1]
+				part2 += history[i][0]
+	print(f'{prefix} part 1: {part1}')
+	print(f'{prefix} part 2: {part2}')
+
+
+if __name__ == '__main__':
+	aoc(get_example_file(), 'Example')
+	aoc(get_input_file(), 'Solution')
