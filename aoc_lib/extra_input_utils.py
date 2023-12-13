3a6f7c6aaa0923642b6b2ec87a005188dd02a717
Peer de Bakker <pde-bakk@student.codam.nl>
2023-12-13 11:10:55 +0100

    made split_on_double_newlines return a list instead

--- aoc_lib/extra_input_utils.py
+++ aoc_lib/extra_input_utils.py
@@ -1,2 +1,5 @@
-def split_on_double_newlines_instead(lines: list[str]) -> list[str]:
-	return '\n'.join(lines).split('\n\n')
+from typing import List
+
+
+def split_on_double_newlines_instead(lines: List[str]) -> List[List[str]]:
+	return [line.split('\n') for line in '\n'.join(lines).split('\n\n')]
