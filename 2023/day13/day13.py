aa409832acf1496f898c362dbc267bdf99abd755
Peer de Bakker <pde-bakk@student.codam.nl>
2023-12-13 10:00:28 +0100

    part2

--- 2023/day13/day13.py
+++ 2023/day13/day13.py
@@ -7,17 +7,19 @@ from aoc_lib.utilities import *
 from aoc_lib.extra_input_utils import split_on_double_newlines_instead
 
 
-def check_equality(p: np.ndarray, x: int) -> bool:
+def check_equality(p: np.ndarray, x: int, p2) -> bool:
 	width, height = len(p[0]), len(p)
 	length = min(x, width - x)
 	arr = [p[y][x + x2] == p[y][x - x2 - 1] for y in range(height) for x2 in range(length)]
+	if p2:
+		return len(arr) - sum(arr) == 1
 	return all(arr)
 
 
-def check_for_i(p: np.ndarray):
+def check_for_i(p: np.ndarray, p2):
 	width, height = len(p[0]), len(p)
 	for i in range(1, width):
-		if check_equality(p, i):
+		if check_equality(p, i, p2):
 			print(f'equal for {i=}')
 			return i
 	return 0
@@ -30,10 +32,15 @@ def aoc(lines: list[str], prefix: str) -> None:
 	for i, p in enumerate(patterns):
 		print(p)
 		data = np.array([list(r) for r in p])
-		horizontal = check_for_i(data)
-		vertical = check_for_i(data.T)
+		horizontal = check_for_i(data, False)
+		vertical = check_for_i(data.T, False)
 		print(f'For pattern {i}, {horizontal=}, {vertical=}')
 		part1 += horizontal + 100 * vertical
+
+		horizontal = check_for_i(data, True)
+		vertical = check_for_i(data.T, True)
+		part2 += horizontal + 100 * vertical
+
 	print(f'{prefix} part 1: {part1}')
 	print(f'{prefix} part 2: {part2}')
 
