d5d7b232fbf241abb4e12be2e1e46679903a0d93
pde-bakk <pde-bakk@student.codam.nl>
2024-12-07 12:07:17 +0100

    made the int concatenation its own seperate function for future use

--- 2024/day07/day07.py
+++ 2024/day07/day07.py
@@ -1,6 +1,7 @@
 import itertools
 import sys
 import math
+from cmath import log10
 
 sys.path.append('../..')
 from aoc_lib.get_input import get_input_file, get_example_file
@@ -24,15 +25,15 @@ def solve(l: list[int], ops: tuple[str, ...]) -> int:
 		elif op == '*':
 			total *= item
 		elif op == '|':
-			total = int(str(total) + str(item))
+			total = concatenate_ints(total, item)
+			# total = int(str(total) + str(item))
 	return total
 
 
 def solve_day07(lines: list[list[int]], allowed_operators: str) -> int:
 	total = 0
 	for result, *items in lines:
-		product = list(itertools.product(allowed_operators, repeat=len(items)-1))
-		for operators in product:
+		for operators in itertools.product(allowed_operators, repeat=len(items)-1):
 			r2 = solve(items, operators)
 			if r2 == result:
 				total += result
--- aoc_lib/utilities.py
+++ aoc_lib/utilities.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 
@@ -37,3 +38,8 @@ def replace_all(s: str, chars_to_replace: str, replacement: str = '') -> str:
 	for c in chars_to_replace:
 		s = s.replace(c, replacement)
 	return s
+
+def concatenate_ints(a: int, b: int) -> int:
+	# Could do it with int(str(a) + str(b)), but that is slow
+	return a * 10 * math.ceil(math.log10(b + 1)) + b
+
