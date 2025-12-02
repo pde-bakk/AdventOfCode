f148dabd19481bb1085492708f77eb7d52781ee1
Peer de Bakker <36886300+pde-bakk@users.noreply.github.com>
2025-12-02 11:46:09 +0100

    2025 day 2

---
+++ 2025/day02/day02.py
@@ -0,0 +1,43 @@
+import sys
+from typing import Tuple
+
+import math
+
+sys.path.append('../..')
+from aoc_lib.get_input import get_input_file, get_example_file
+from aoc_lib.utilities import *
+from aoc_lib.extra_input_utils import *
+from aoc_lib.directions import *
+
+
+def parse(data: str) -> list[str]:
+	lines = data.rstrip('\n').split(',')
+	return lines
+
+
+def solve(lines: list[str]) -> Tuple[int, int]:
+	p1 = p2 = 0
+	for line in lines:
+		a, b = map(int, line.split('-'))
+		for x in range(a, b + 1):
+			s = str(x)
+			if 2 * str(s[:len(s)//2]) == s:
+				p1 += x
+			for l in range(1, len(s)):
+				if s[:l] * (len(s) // l) == s:
+					p2 += x
+					break
+	return p1, p2
+
+
+def aoc(data: str, args, prefix: str) -> None:
+	lines = parse(data)
+	part1, part2 = solve(lines)
+	print(f'{prefix} part 1: {part1}, part 2: {part2}')
+
+if __name__ == '__main__':
+	program_args = parse_args()
+	if 'example' in program_args.mode:
+		aoc(get_example_file(), program_args, 'Example')
+	if 'solution' in program_args.mode:
+		aoc(get_input_file(), program_args, 'Solution')
