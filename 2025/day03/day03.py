2e8670afe8a2232770fe8861befa0795c784a6ed
pde-bakk <pde-bakk@student.codam.nl>
2025-12-03 13:08:03 +0100

    had a bug in part 2 when i == 0

---
+++ 2025/day03/day03.py
@@ -0,0 +1,43 @@
+import sys
+import math
+from typing import Tuple
+
+sys.path.append('../..')
+from aoc_lib.get_input import get_input_file, get_example_file
+from aoc_lib.utilities import *
+from aoc_lib.extra_input_utils import *
+from aoc_lib.directions import *
+
+
+def parse(data: str) -> list[str]:
+	lines = split_data_on_newlines(data)
+	return lines
+
+
+def solve(lines: list[str]) -> Tuple[int, int]:
+	p1 = p2 = 0
+	for line in lines:
+		first = max(line[:-1])
+		second = max(line[line.index(first)+1:])
+		p1 += int(first + second)
+		l = []
+		for i in range(11, -1, -1):
+			item = max(line) if i == 0 else max(line[:-i])
+			l.append(item)
+			line = line[line.index(item)+1:]
+		p2 += int(''.join(l))
+	return p1, p2
+
+
+def aoc(data: str, _, prefix: str) -> None:
+	lines = parse(data)
+	part1, part2 = solve(lines)
+	print(f'{prefix} part 1: {part1} and part 2: {part2}')
+
+
+if __name__ == '__main__':
+	program_args = parse_args()
+	if 'example' in program_args.mode:
+		aoc(get_example_file(), program_args, 'Example')
+	if 'solution' in program_args.mode:
+		aoc(get_input_file(), program_args, 'Solution')
